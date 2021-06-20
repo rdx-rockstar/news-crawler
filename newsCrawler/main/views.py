a683e20108cef46bfb7f5468b33a0dbf97829525
Mrinal Kothari <mrinalkothari@Mrinals-MacBook-Air.local>
2021-06-20 22:56:19 +0530

    Sign Up page updated

--- newsCrawler/main/views.py
+++ newsCrawler/main/views.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import User,auth
 
 def home(request):
     return render(request,'home.html',{'user':request.user})
+
+def register(request):
+    return render(request,'register.html',{'user':request.user})
